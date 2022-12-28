a63b990d03dda90983fd14d481e55dce29892a61
scottohalloran <o_halloran@yahoo.com>
2022-12-27 21:56:53 -0800

    Added shell scripts and buildspec.yml

--- scripts/create-backend-s3.sh
+++ scripts/create-backend-s3.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Create the S3 bucket in which to store the Terraform TFState
 aws s3api create-bucket --bucket scottohallorancv.com-tfstate
+# Enable S3 versioning on the TF State bucket
+aws s3api put-bucket-versioning --bucket scottohallorancv.com-tfstate --versioning-configuration Status=Enabled
